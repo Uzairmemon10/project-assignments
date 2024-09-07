807f7207f7890734721d299d205b661c45a00712
Uzairmemon10 <um889943@gmail.com>
2024-09-07 13:55:11 -0700

    added file

---
+++ jprject.js
@@ -0,0 +1,21 @@
+let grade = prompt("enter your percentage")
+
+if (grade < 0 || grade > 100){
+ console.log("percentage  is uncorrect");
+}
+else  if (grade >= 80  ) {
+    console.log("A1 GRADE");
+}
+else if (grade >= 70) {
+    console.log("A GRADE");
+}
+else if (grade >= 60) {
+    console.log("B GRADE");
+}
+else if (grade >= 50) {
+    console.log("C GRADE");
+}
+ else (console.log("you are fail")); 
+    
+
+
