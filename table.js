153da5aed5f3090b3d6232a5f2852b2f509899ad
Uzairmemon10 <um889943@gmail.com>
2024-09-25 14:45:08 -0700

    by me

---
+++ table.js
@@ -0,0 +1,11 @@
+
+let table = +prompt("enter any num")
+let num = 5;
+for (let i = 0; i <= 10; i++) {
+    if (table == 0){
+        console.log(num + "x" + i + "=" + i * num)
+    } else{
+        console.log(table + "X" + i + "=" + i * table)
+    }
+}
+
